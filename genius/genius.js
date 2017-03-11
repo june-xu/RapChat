96ce9fe64a44fa636224665b8c5a55ce51c6eb46
june-xu <junehqxu@gmail.com>
2017-03-11 17:40:11 -0500

    Added genius folder

---
+++ genius/genius.js
@@ -0,0 +1,22 @@
+usClient = require("rapgenius-js");
+
+rapgeniusClient.searchArtist("GZA", "rap", function(err, artist){
+  if(err){
+    console.log("Error: " + err);
+  }else{
+    console.log("Rap artist found [name=%s, link=%s, popular-songs=%d]",
+                artist.name, artist.link, artist.popularSongs.length);
+
+  }
+});
+
+//Example for a rock artist
+rapgeniusClient.searchArtist("Bruce Springsteen", "rock", function(err, artist){
+  if(err){
+    console.log("Error: " + err);
+  }else{
+    console.log("Rap artist found [name=%s, link=%s, popular-songs=%d]",
+                artist.name, artist.link, artist.popularSongs.length);
+
+  }
+});
