bb31b7fd6d1a4dc0e488a3ea39deb1a7f5394e8d
torvarun <torvarun@gmail.com>
2017-03-11 20:54:36 -0500

    rhymezone

--- libs/rhyme.js
+++ libs/rhyme.js
@@ -18,7 +18,7 @@ function getRhymezone(text){
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-    console.log('body:', body); // Print the response
+    console.log('body text:', body); // Print the response
 
     return body;
   });
