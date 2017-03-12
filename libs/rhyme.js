8be39ed66a7c38b29aeb1acbfa7785767c7a1b9b
torvarun <torvarun@gmail.com>
2017-03-11 20:18:57 -0500

    rhymezone

--- libs/rhyme.js
+++ libs/rhyme.js
@@ -11,18 +11,16 @@ function getRhymes(text){
 function getRhymezone(text){
   console.log("Rhymezone: " + text);
 
-  String url = "https://api.datamuse.com/words?rel_rhy=" + text
+  var resp = "";
 
-  var text = "";
-
-  request(url, function (error, response, body) {
+  request("https://api.datamuse.com/words?rel_rhy=" + text, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the response
-    text = body;
+    resp = body;
   });
 
-  return text;
+  return resp;
 
 }
 
