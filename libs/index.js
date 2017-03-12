dd1507c3cd0fe7bf967ffe2464ff3788903df6b5
mokaixu <mokaixu@gmail.com>
2017-03-12 15:22:42 -0400

    updated dependencies and restructure xmas directory

--- libs/index.js
+++ libs/index.js
@@ -4,10 +4,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+const action_handler = require('./action_handler.js');
 
 const PAGE_ACCESS_TOKEN = "EAADndRHzHwoBANXzbdMnEGFD5oVJYzkiUVrVbnZCoM4JnXKiFtanGLvGLKMGDfXlMpIoshnq4W0qX9TgFMQByLXxcgUNpnf9vrpEE8kkYadBpbh3fPZA0PFGxaZAeZAImexfFVZABAkjUALLnGj9OzFwZBb5BNwSSvrZBUpu2sSZBwZDZD"
 
 const rhyme = require('./rhyme.js')
+
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -34,37 +36,33 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
-
+// get request static data
+// post data sed "waiting for response"
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
         if (event.message && event.message.text) {
+
             let text = event.message.text
+<<<<<<< Updated upstream
 
             rhyme.getRhyme(text, function(resp){
               console.log("Final x2: " + resp);
               sendTextMessage(sender, resp);
             })
+=======
+            action_handler.handleMessageReceive(sender, text);
+
+>>>>>>> Stashed changes
         }
     }
     res.sendStatus(200)
 })
 
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
 
-  callSendAPI(messageData);
-}
-
+//API call to FB chatbot
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
