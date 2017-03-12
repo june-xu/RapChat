8d991786e199e98ebf1d7b2422f1886b76c47903
torvarun <torvarun@gmail.com>
2017-03-12 03:52:14 -0400

    lyrics

--- libs/rhyme.js
+++ libs/rhyme.js
@@ -60,7 +60,7 @@ function queryLine(id){
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-    console.log(body);
+    //console.log(body);
     body = body.substring(9, body.length -2);
     let jsonResp = JSON.parse(body);
 
