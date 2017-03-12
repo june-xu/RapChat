c76d8a79ee04e845b0734fe17c8de8f8f67e7162
torvarun <torvarun@gmail.com>
2017-03-11 20:27:55 -0500

    rhymezone

--- libs/rhyme.js
+++ libs/rhyme.js
@@ -13,6 +13,7 @@ function getRhymezone(text){
 
   var resp = "";
 
+  console.log("URL = " + "https://api.datamuse.com/words?rel_rhy=" + text );
   request("https://api.datamuse.com/words?rel_rhy=" + text, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
